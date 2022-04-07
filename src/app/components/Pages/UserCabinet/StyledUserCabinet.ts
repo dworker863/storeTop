dc8f532c46d17483afcbbd4245a95efb51e2ba93
dworker <dworker863@gmail.com>
2022-04-07 17:24:40 +0600

    Desktop. Changed UserCabinet component

--- src/app/components/Pages/UserCabinet/StyledUserCabinet.ts
+++ src/app/components/Pages/UserCabinet/StyledUserCabinet.ts
@@ -1,13 +1,101 @@
 import styled from 'styled-components';
 
-export const StyledUserCabinet = styled.section``;
+export const StyledUserCabinet = styled.section`
+  width: 100%;
+  color: ${(props) => props.theme.colors.grey};
+`;
 
-export const StyledUserCabinetName = styled.p``;
+export const StyledUserCabinetInfoWrapper = styled.div`
+  display: flex;
+  margin-top: 35px;
+  border-radius: 6px;
+  border: 1px solid ${(props) => props.theme.colors.lightGrey};
+`;
 
-export const StyledUserCabinetTel = styled.p``;
+export const StyledUserCabinetPhotoWrapper = styled.div`
+  padding: 35px;
+  border-right: 1px solid ${(props) => props.theme.colors.lightGrey};
 
-export const StyledUserCabinetEmail = styled.p``;
+  button {
+    &:nth-child(2) {
+      margin-right: 12px;
+    }
+  }
+`;
 
-export const StyledUserCabinetAddress = styled.p``;
+export const StyledUserCabinetPhoto = styled.div`
+  width: 380px;
+  height: 455px;
+  margin-bottom: 20px;
+  border: 1px solid ${(props) => props.theme.colors.lightGrey};
+  border-radius: 4px;
+  background-color: ${(props) => props.theme.colors.superLightGrey};
+`;
 
-export const StyledUserCabinetPostIndex = styled.p``;
+export const StyledUserCabinetInfo = styled.div`
+  padding: 30px 45px;
+`;
+
+export const StyledUserCabinetName = styled.h3`
+  margin-top: 0;
+  margin-bottom: 15px;
+  font-size: 24px;
+`;
+
+export const StyledUserCabinetDate = styled.p`
+  margin-top: 0;
+  width: max-content;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 1px;
+    margin-top: 10px;
+    margin-bottom: 20px;
+    background-color: ${(props) => props.theme.colors.lightGrey};
+  }
+`;
+
+export const StyledUserCabinetEmail = styled.p`
+  &:after {
+    content: '';
+    display: block;
+    width: 105px;
+    height: 1px;
+    margin-top: 10px;
+    background-color: ${(props) => props.theme.colors.lightGrey};
+  }
+`;
+
+export const StyledUserCabinetSubtitle = styled.h4`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  font-size: 16px;
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.primary};
+`;
+
+export const StyledUserCabinetTel = styled.p`
+  &:after {
+    content: '';
+    display: block;
+    width: 105px;
+    height: 1px;
+    margin-top: 10px;
+    background-color: ${(props) => props.theme.colors.lightGrey};
+  }
+`;
+export const StyledUserCabinetNote = styled.p`
+  max-width: 400px;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${(props) => props.theme.colors.lightGrey};
+`;
+
+export const StyledUserCabinetAddressWrapper = styled.div``;
+
+export const StyledUserCabinetAddress = styled.p`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
--- src/app/components/Pages/UserCabinet/UserCabinet.tsx
+++ src/app/components/Pages/UserCabinet/UserCabinet.tsx
@@ -1,13 +1,23 @@
 import { FC } from 'react';
 import Container from '../../Blocks/Container/Container';
+import Button from '../../Elements/Button/Button';
+import { StyledTextGrey } from '../../Elements/ContactsItem/StyledContactsItem';
 import SectionTitle from '../../Elements/SectionTitle/SectionTitle';
+import TextOrange from '../../Elements/TextOrange/TextOrange';
 import { IUserCabinet } from './IUserCabainet';
 import {
   StyledUserCabinet,
   StyledUserCabinetAddress,
+  StyledUserCabinetAddressWrapper,
+  StyledUserCabinetDate,
   StyledUserCabinetEmail,
+  StyledUserCabinetInfo,
+  StyledUserCabinetInfoWrapper,
   StyledUserCabinetName,
-  StyledUserCabinetPostIndex,
+  StyledUserCabinetNote,
+  StyledUserCabinetPhoto,
+  StyledUserCabinetPhotoWrapper,
+  StyledUserCabinetSubtitle,
   StyledUserCabinetTel,
 } from './StyledUserCabinet';
 
@@ -15,16 +25,58 @@ const UserCabinet: FC<IUserCabinet> = ({ user }) => {
   return (
     <Container type="common">
       <StyledUserCabinet>
-        <SectionTitle text="UserCabinet" primary={false} />
-        <StyledUserCabinetName>Имя: {user.username}</StyledUserCabinetName>
-        <StyledUserCabinetTel>Телефон: {user.tel}</StyledUserCabinetTel>
-        <StyledUserCabinetEmail>Email: {user.email}</StyledUserCabinetEmail>
-        <StyledUserCabinetAddress>
-          Адрес: {user.address}
-        </StyledUserCabinetAddress>
-        <StyledUserCabinetPostIndex>
-          Почтовый индекс: {user.postIndex}
-        </StyledUserCabinetPostIndex>
+        <SectionTitle text="Личный кабинет" primary={false} />
+        <StyledUserCabinetInfoWrapper>
+          <StyledUserCabinetPhotoWrapper>
+            <StyledUserCabinetPhoto></StyledUserCabinetPhoto>
+            <Button text="Изменить изображение" />
+            <Button text="X Удалить" />
+          </StyledUserCabinetPhotoWrapper>
+
+          <StyledUserCabinetInfo>
+            <StyledUserCabinetName>{user.username}</StyledUserCabinetName>
+            <StyledUserCabinetDate>
+              <TextOrange text="Дата регистрации на сайте:" /> 20 апреля 2022 г.
+            </StyledUserCabinetDate>
+            <StyledUserCabinetEmail>
+              <StyledUserCabinetSubtitle>
+                Адрес электронной почты
+              </StyledUserCabinetSubtitle>
+              {user.email}
+            </StyledUserCabinetEmail>
+            <StyledUserCabinetTel>
+              <StyledUserCabinetSubtitle>
+                Контактный номер телефона
+              </StyledUserCabinetSubtitle>
+              {user.tel}
+              <StyledUserCabinetNote>
+                <StyledTextGrey>Примечание:</StyledTextGrey>
+                <br />
+                Просим Вас указывать действительный номер телефона для связи.
+                Номер телефона используется только для связи с клиентом в целях
+                подтвержения заказа товара
+              </StyledUserCabinetNote>
+            </StyledUserCabinetTel>
+            <StyledUserCabinetAddressWrapper>
+              <StyledUserCabinetSubtitle>
+                Адрес и почтовый индекс
+              </StyledUserCabinetSubtitle>
+              Республика Казахстан,
+              <br />
+              <StyledUserCabinetAddress>г. Алматы</StyledUserCabinetAddress>
+              <StyledUserCabinetAddress>
+                ул. Уличная, д. 50А, кв. 111, 7 этаж
+              </StyledUserCabinetAddress>
+              Почтовый индекс: {user.postIndex}
+              <StyledUserCabinetNote>
+                <StyledTextGrey>Примечание:</StyledTextGrey>
+                <br />
+                Адрес используется только для доставки курьером или опредения
+                ближней почтовой точки для доставки.
+              </StyledUserCabinetNote>
+            </StyledUserCabinetAddressWrapper>
+          </StyledUserCabinetInfo>
+        </StyledUserCabinetInfoWrapper>
       </StyledUserCabinet>
     </Container>
   );
